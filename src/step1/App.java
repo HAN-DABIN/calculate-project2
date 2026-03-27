c0a90d02e8cbe11efb14002bc3d54d35a4231a98
HAN-DABON <sinmint11@gmail.com>
2026-03-27 16:00:58 +0900

    연산자 로직 구현

---
+++ src/step1/App.java
@@ -0,0 +1,60 @@
+package step1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        // 스캐너 선언
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("계산기를 실행합니다.");
+
+        // 계산기 전체 반복문
+        while (true) {
+            int num1; // 첫 번째 숫자 입력값 담을 변수 선언 -> 반복문 때문에 먼저 선언
+            // 첫 번째 숫자 반복문
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력해주세요.: ");
+                try {
+                    num1 = scanner.nextInt(); // num1 입력 받기
+                    scanner.nextLine(); // 개행문자 제거
+                    if (num1 < 0) { // 입력한 숫자가 음의 정수일 때 문장 출력
+                        System.out.println("양의 정수를 입력해주세요");
+                        break; // 무한루프 방지
+                    }
+                    } catch (InputMismatchException e){ // int(정수) 외 입력 시 예외처리
+                        System.out.println("정수를 입력해주세요.");
+                        scanner.nextLine(); // 개행문자 제거
+                    continue;
+                    }
+                break; // 첫 번째 숫자 입력 반복문 종료
+                }
+
+            char ope; // 연산자 담을 변수 선언
+            // 연산자 입력 반복문
+            while (true) {
+                System.out.print("연산자(+, -, *, /)를 입력해주세요.: ");
+               ope = scanner.next().charAt(0); // ope 입력받기
+                // 사칙연산 외 다른문자 입력 시 문구 출력
+                    switch (ope) {
+                        case '+', '-', '*', '/':
+                            break;
+                        default:
+                            System.out.println("사칙연산 중 하나를 입력해주세요.");
+                            continue;
+                    }
+                    break; // 연산자 입력 반복문 종료
+                    }
+
+
+
+                    }
+
+                }
+            }
+
+
+
+
+
