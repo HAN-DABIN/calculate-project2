5087d5c375a01b8d7c21d7c0f3a9bc7dcb002acc
HAN-DABON <sinmint11@gmail.com>
2026-03-27 20:02:08 +0900

    step2 계산하기-첫 번째 숫자입력

---
+++ src/step2/App.java
@@ -0,0 +1,31 @@
+package step2;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        // 스캐너 선언
+        Scanner scanner= new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.println("=== 계산기 메뉴 ===");
+        System.out.println("1. 계산하기");
+        System.out.println("2. 계산 기록 보기");
+        System.out.println("3. 계산 기록 삭제하기");
+        System.out.println("0. 계산기 종료");
+
+        int select = scanner.nextInt();
+
+        if (select == 1) {
+            System.out.println("=== 계산을 실행합니다 ===");
+            // 계산하기 반복문
+            while (true) {
+                int num1 = calculator.firstNum(scanner, "- 첫 번째 숫자를 입력하세요.: ");
+
+            }
+
+        }
+
+    }
+}
---
+++ src/step2/Calculator.java
@@ -0,0 +1,33 @@
+package step2;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator {
+    // 연산 결과 저장 컬렉션 - 캡슐화
+    private ArrayList<Integer> resultList = new ArrayList<>();
+
+    // 첫 번째 숫자 입력 메서드
+    public int firstNum(Scanner scanner, String first) {
+        int num1;
+        while (true) {
+            // first에 담긴 문장 출력
+            System.out.println(first);
+            try {
+                // num1에 입력 값 저장
+                num1 = scanner.nextInt(); // num1 입력 받기
+                scanner.nextLine(); // 개행문자 제거
+                if (num1 < 0) { // 입력한 숫자가 음의 정수일 때 문장 출력
+                    System.out.println("※ 양의 정수를 입력해주세요.");
+                    continue; // 무한루프 방지
+                }
+            } catch (InputMismatchException e) { // int(정수) 외 입력 시 예외처리
+                System.out.println("※ 정수를 입력해주세요.");
+                scanner.nextLine(); // 개행문자 제거
+                continue;
+            }
+            break; // 첫 번째 숫자 입력 반복문 종료
+        } return num1;
+    }
+}
