131776d4b8acf0a21fa415f89651d03511316212
HAN-DABON <sinmint11@gmail.com>
2026-03-28 11:50:26 +0900

    Step2 완료

--- src/step2/App.java
+++ src/step2/App.java
@@ -16,6 +16,7 @@ public class App {
             System.out.println("2. 계산 기록 보기");
             System.out.println("3. 첫 번째 계산 기록 삭제하기");
             System.out.println("0. 계산기 종료");
+            System.out.println("< 메뉴를 숫자로 입력해주세요. >");
 
             int select = scanner.nextInt();
 
@@ -23,12 +24,14 @@ public class App {
                 System.out.println("=== 계산을 실행합니다 ===");
                 // 계산하기 반복문
                 while (true) {
+                    // 각 메서드 호출
                     int num1 = calculator.firstNum(scanner, "- 첫 번째 숫자를 입력해주세요.: ");
                     char ope = calculator.ope(scanner, "- 연산자(+, -, *, /)를 입력해주세요.: ");
                     int num2 = calculator.secondNum(scanner, "- 두 번째 숫자를 입력해주세요.: ", ope);
-                    int result = calculator.cacaulate(num1, ope, num2);
+                    int result = calculator.calculate(num1, ope, num2);
                     String ans = calculator.answer(scanner, "- 다시 계산하시겠습니까? (yes: 다시계산 / menu: 메뉴로 돌아가기 / exit: 계산기 종료)");
 
+                    // ans의 입력값이 "menu"라면 계산하기 반복문 종료
                     if (ans.equals("menu")) {
                         break;
                     }
@@ -41,7 +44,21 @@ public class App {
                 System.out.println("계산 결과: " + resultList);
                 String gM = calculator.goMenu(scanner, "- 메뉴로 돌아가시겠습니까? (yes: 메뉴로 돌아가기 / exit: 계산기 종료)");
             }
-
+            if (select == 3) {
+                System.out.println("=== 첫 번째 계산 기록을 삭제합니다.===");
+                // getter 활용
+                ArrayList<Integer> resultList = calculator.getResultList();
+                System.out.println("기존 계산 기록: " + resultList);
+                // removeResult 메서드 호출
+                calculator.removeResult();
+                String gM = calculator.goMenu(scanner, "- 메뉴로 돌아가시겠습니까? (yes: 메뉴로 돌아가기 / exit: 계산기 종료)");
+            }
+            if (select == 0) {
+                System.out.println("=== 프로그램을 종료합니다. ===");
+                System.exit(0);
+            } else {
+                System.out.println("※ 알맞은 메뉴 번호를 입력해주세요.");
+            }
         }
     }
 }
--- src/step2/Calculator.java
+++ src/step2/Calculator.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class Calculator {
     // 연산 결과 저장 컬렉션 - 캡슐화
-    private ArrayList<Integer> resultList = new ArrayList<>();
+    private ArrayList<Integer> resultList = new ArrayList<>(); // 속성
 
-    // 첫 번째 숫자 입력 메서드
+    // 첫 번째 숫자 입력 메서드 (기능)
     public int firstNum(Scanner scanner, String first) {
         int num1;
         while (true) {
             // first에 담긴 문장 출력
-            System.out.println(first);
+            System.out.print(first);
             try {
                 // num1에 입력 값 저장
                 num1 = scanner.nextInt(); // num1 입력 받기
@@ -31,11 +31,11 @@ public class Calculator {
         } return num1;
     }
 
-    // 연산자 입력 메서드
+    // 연산자 입력 메서드 (기능)
     public char ope (Scanner scanner, String operator) {
         char ope2;
         while (true) {
-            System.out.println(operator);
+            System.out.print(operator);
             ope2 = scanner.next().charAt(0); // ope 입력받기
             // 사칙연산 외 다른문자 입력 시 문구 출력
             switch (ope2) {
@@ -49,11 +49,11 @@ public class Calculator {
         } return ope2;
     }
 
-    // 두 번째 숫자 입력 메서드
+    // 두 번째 숫자 입력 메서드 (기능)
     public int secondNum (Scanner scanner, String second, char ope) {
         int num2;
         while (true) {
-            System.out.println(second);
+            System.out.print(second);
             try {
                 num2 = scanner.nextInt();
                 scanner.nextLine(); // 개행문자 제거
@@ -75,30 +75,31 @@ public class Calculator {
         } return num2;
     }
 
-    // 계산 메서드
-    public int cacaulate(int num1, char ope, int num2) {
+    // 계산 메서드 (기능)
+    public int calculate(int num1, char ope, int num2) {
         int result = 0;
         switch (ope) {
             case '+':
                 result = num1 + num2;
-                System.out.println("::: " + num1 + " + " + num2 + " = " + result);
+                System.out.println("* 계산 결과: " + num1 + " + " + num2 + " = " + result);
                 break;
             case '-':
                 result = num1 - num2;
-                System.out.println("::: " + num1 + " - " + num2 + " = " + result);
+                System.out.println("* 계산 결과: " + num1 + " - " + num2 + " = " + result);
                 break;
             case '*':
                 result = num1 * num2;
-                System.out.println("::: " + num1 + " * " + num2 + " = " + result);
+                System.out.println("* 계산 결과: " + num1 + " * " + num2 + " = " + result);
                 break;
             case '/':
                 result = num1 / num2;
-                System.out.println("::: " + num1 + " / " + num2 + " = " + result);
+                System.out.println("* 계산 결과: " + num1 + " / " + num2 + " = " + result);
                 break;
         }
         resultList.add(result);
         return result;
     }
+    // 계속 계산 입력 메서드 (기능)
     public String answer(Scanner scanner, String answer2) {
         String ans;
         while (true) {
@@ -125,10 +126,12 @@ public class Calculator {
     public ArrayList<Integer> getResultList() {
         return this.resultList;
     }
-
+    // 메뉴 복귀 메서드 (기능)
     public String goMenu(Scanner scanner, String goMenu2) {
         String gM;
+        // 메뉴 입력 반복문
         while (true) {
+            // 메뉴로 돌아가는 입력 받기
             System.out.println(goMenu2);
             scanner.nextLine(); // 개행문자 제거 => 전 입력이 String이었는데 왜 생겼을까? * 트러블슈팅
             gM = scanner.nextLine();
@@ -144,6 +147,15 @@ public class Calculator {
                     continue;
             } break;
         } return gM;
+    }
+    // 첫 번째 계산 기록 (0번째 인덱스) 삭제 메서드 (기능)
+    public void removeResult() {
+        if (!resultList.isEmpty()) {
+            resultList.remove(0);
+            System.out.println("변경 계산 기록: " + resultList);
+        } else {
+            System.out.println("※ 삭제할 결과가 없습니다.");
+        }
     }
 }
 
